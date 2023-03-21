e76f3af266f074e72a8f1cccda33d75441576260
Hizi70k <95754469+Hizi70k@users.noreply.github.com>
2023-03-21 13:39:42 +0800

    Update pagerank.py
    short optimization in mar 21

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -16,6 +16,7 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python pagerank.py corpus")
     corpus = crawl(sys.argv[1])
+    print(corpus)
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
@@ -41,7 +42,9 @@ def crawl(directory):
         with open(os.path.join(directory, filename)) as f:
             contents = f.read()
             links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
-            pages[filename] = set(links) - {filename}
+            pages[filename] = set(links)
+            # Error with the given code: 
+            # pages[filename] = set(links) - {filename}
 
     # Only include links to other pages in the corpus
     for filename in pages:
@@ -96,43 +99,26 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    ####################
-    ## YOUR CODE HERE ##
-    ####################
-
     # Powered by Hu Zhengtao
     
     pr = dict.fromkeys( list( corpus.keys() ) ,0)
 
     random.seed( time.time())
-    num = 0
-    pages = {}
-    for i in corpus.keys():
-        pages[num] = i
-        num = num + 1
-    def rd_page():        
-        rd = random.randint(0,num-1)
-        return pages[ rd ]    
 
-    pl = rd_page()
+    N = len( corpus.keys() )
+    pl = random.choice(list(corpus.keys())) 
     for i in range(n):
-        rdm = random.random()
-        if rdm <= damping_factor:
-            while ( a := rd_page() )  not in corpus[pl]:
-                pass
-            pr[pl := a] += 1
+        if random.random() <= damping_factor:
 
-            # With Low Efficiency!!! 
-            # Every random element of set cost much
+            pl = random.choice(list(corpus[pl]))
+            pr[pl] += 1
         else:       
-            pl = rd_page()
+            pl = random.choice(list(corpus.keys()))
             pr[pl] += 1
 
     for i in pr:
         pr[i] = pr[i] / n
     return pr
-
-
 
 def iterate_pagerank(corpus, damping_factor):
     """
@@ -143,32 +129,31 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    ####################
-    ## YOUR CODE HERE ##
-    ####################
 
     N = len( corpus )
-    pr = dict.fromkeys( list( corpus.keys() ) ,1/N)
-
-    linkin = {}
-    for i in corpus.keys():
-        for j in corpus[i]:
-            if j not in linkin.keys():
-                linkin[j] = []
-            linkin[j].append(i)
+    pr = dict.fromkeys( list( corpus.keys() ) ,1.0/N)
 
     flag = True
     while flag:
         p2 = {}
-        for i in pr.keys():
-            p2[i] = (1-damping_factor) / N + damping_factor * sum(  pr[j] / len( corpus[j] ) for j in linkin[i] ) 
-
         flag = False
+        
+        for page in pr.keys():
+            p2[page] = (1.0-damping_factor) / N + damping_factor * get_sum(corpus, pr, page)
+
         for i in pr.keys():
             if abs( pr[i] - p2[i] ) > 0.001:   flag = True
         pr = p2
+
     return pr
 
+
+def get_sum(corpus, distribution, page):
+    result = 0
+    for p in corpus:
+        if page in corpus[p]:
+            result += distribution[p] / len(corpus[p])
+    return result
 
 if __name__ == "__main__":
     main()
